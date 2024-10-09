dea2b4bd702a159c2d3cfc951a4821fe5cf55892
cmseric <512315211@qq.com>
2024-10-09 14:15:38 +0800

    init: sign in system

---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import {
+  convexAuthNextjsMiddleware,
+  createRouteMatcher,
+  nextjsMiddlewareRedirect,
+  isAuthenticatedNextjs
+} from "@convex-dev/auth/nextjs/server";
+ 
+const isPublicPage = createRouteMatcher(["/auth"]);
+ 
+export default convexAuthNextjsMiddleware((request) => {
+  console.log(isAuthenticatedNextjs())
+  if (!isPublicPage(request) && !isAuthenticatedNextjs()) {
+    return nextjsMiddlewareRedirect(request, "/auth");
+  }
+
+  if (isPublicPage(request) && isAuthenticatedNextjs()) {
+    return nextjsMiddlewareRedirect(request, "/");
+  }
+});
+ 
+export const config = {
+  // The following matcher runs middleware on all routes
+  // except static assets.
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
